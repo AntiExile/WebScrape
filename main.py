371859786f75ce7a502e257f8a8d33ad248d2790
AntieDev <wianmarais122007@gmail.com>
2025-01-23 22:07:48 +0200

    Version 0.2

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import QPropertyAnimation, QEasingCurve
 from src.gui.main_window import MainWindow
 from src.utils.theme_manager import ThemeManager
+from PyQt6.QtGui import QIcon
 
 def main():
     app = QApplication(sys.argv)
     theme_manager = ThemeManager()
+    app.setWindowIcon(QIcon("path/to/your/icon.png"))
     window = MainWindow(theme_manager)
     window.show()
     sys.exit(app.exec())
